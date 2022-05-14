e49a4ec7d58e9673a5023d0f8108157cf9ee98ef
chandler-pc <chandler.perez.c@uni.pe>
2022-05-13 21:13:03 -0500

    Re-Struct

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include "Game.h"
-#include "Button.h"
-
-void Menu();
+#include "Source/Public/Menu.h"
 
 int main() {
-	Game game;
-	game.Run();
-	//Menu();
+	Menu menu;
+	menu.Run();
 	return 0;
-}
-
-void Menu() {
-	sf::RenderWindow menuWin(sf::VideoMode(800, 800), "Menu", sf::Style::Close);
-	menuWin.setFramerateLimit(60);
-	while (menuWin.isOpen()) {
-		sf::Event eventmenu;
-		if (menuWin.pollEvent(eventmenu)) {
-			if (eventmenu.type == sf::Event::Closed) {
-				menuWin.close();
-			}
-		}
-		menuWin.clear(sf::Color::Black);
-		menuWin.display();
-	}
 }
